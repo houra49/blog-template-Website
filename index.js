2aa8d17406e2b06ef6dbe28834f4a300ef196d29
houra49 <houra.shamshyrcar@gmail.com>
2021-04-12 14:54:20 +0200

    blog-template-Website

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+const infos = require('./infos.json');
+app.use(express.static('public'))
+app.listen(3000, () => {
+    console.log('listening at http://localhost:3000')
+})
+app.set('view engine', 'ejs')
+app.use(express.urlencoded({ extended: false }))
+app.use(express.json())
+app.get('/', (req, res) => {
+    res.render('pages/index',
+        {
+            title: 'Homepage',
+            data: infos
+        })
+})
